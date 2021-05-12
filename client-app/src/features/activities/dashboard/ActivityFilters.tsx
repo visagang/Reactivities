f702bb13c2e56279dd13d6591f8e3f61a71dc3de
VSP20SCM31G <62565204+VSP20SCM31G@users.noreply.github.com>
2021-05-12 00:06:21 -0500

    End of Section 9

---
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import Calendar from 'react-calendar';
+import { Header, Menu } from 'semantic-ui-react';
+
+export default function ActivityFilters() {
+
+    return (
+        <Fragment>
+            <Menu vertical size='large' style={{width: '100%', marginTop: 25}}>
+                <Header icon='filter' attached color='teal' content='Filters'/>
+                <Menu.Item content='All Activities'/>
+                <Menu.Item content='I am going'/>
+                <Menu.Item content='I am hosting'/>
+            </Menu>
+            <Header/>
+            <Calendar />
+        </Fragment>
+
+    )
+}
--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,43 +1,46 @@
-import React, { SyntheticEvent, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Item, Button, Label } from 'semantic-ui-react';
+import { Item, Button, Segment, Icon } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
-import { useStore } from '../../../app/stores/store';
 
 interface Props {
     activity: Activity;
 }
 export default function ActivityListItem({activity}: Props) {
-    const {activityStore} = useStore();
-    const [target, setTarget] = useState('');
-    const {deleteActivity, loading} = activityStore;
-
-    function handleDeleteActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-    
+   
     return(
-        <Item key={activity.id}>
-        <Item.Content>
-            <Item.Header as ='a'>{activity.title}</Item.Header>
-            <Item.Meta>{activity.date}</Item.Meta>
-            <Item.Description>
-                <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
-            </Item.Description>
-            <Item.Extra>
-                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue'/>
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png'/>
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' />{activity.date}
+                    <Icon name='marker' />{activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attedes go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
                 <Button 
-                name={activity.id}
-                onClick={(e) => handleDeleteActivity(e,activity.id)} 
-                floated='right' 
-                loading={loading && target === activity.id} 
-                content='Delete' 
-                color='red'/>
-                <Label basic content={activity.category} />
-            </Item.Extra>
-        </Item.Content>
-    </Item>
+                as={Link}
+                to={`/activities/${activity.id}`}
+                color='teal'
+                floated='right'
+                content='View'/>
+            </Segment>
+        </Segment.Group>
     )
 }
