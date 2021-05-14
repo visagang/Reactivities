9ace33c58d31163cc389aab2048a26c1ed12904b
VSP20SCM31G <62565204+VSP20SCM31G@users.noreply.github.com>
2021-05-14 15:24:09 -0500

    Added error handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,25 +3,34 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, useLocation } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetail from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage}/>
       <Route path={'/(.+)'} 
       render={() => (
         <> 
         <NavBar />
       <Container style={{marginTop: '7em'}}>
-        
-        <Route exact path='/activities' component={ActivityDashboard}/>
-        <Route path='/activities/:id' component={ActivityDetail}/>
-        <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
+        <Switch>
+          <Route exact path='/activities' component={ActivityDashboard}/>
+          <Route path='/activities/:id' component={ActivityDetail}/>
+          <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
+          <Route path='/errors' component={TestErrors} />
+          <Route path='/server-error' component={ServerError}/>
+          <Route component={NotFound}/>
+        </Switch>
       </Container>
         </>
       )}
